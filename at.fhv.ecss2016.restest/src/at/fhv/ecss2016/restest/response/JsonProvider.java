f133fc5e0bf80e7c8f874e0a0e6f744165ce846e
msmith <msmith@DESKTOP-GA8D0T4.mshome.net>
2016-05-20 10:48:53 +0200

    updated target platform

--- at.fhv.ecss2016.restest/src/at/fhv/ecss2016/restest/response/JsonProvider.java
+++ at.fhv.ecss2016.restest/src/at/fhv/ecss2016/restest/response/JsonProvider.java
@@ -14,6 +14,8 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+
+
 public class JsonProvider {
 
 	private UrlSource urlSource = new UrlSource();
