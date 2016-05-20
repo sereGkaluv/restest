2c03e62fd78e866dbea86446dca0bc474bc69fcb
sereGkaluv <malicx7@gmail.com>
2016-05-20 09:09:49 +0200

    Finished config part ui

--- at.fhv.ecss2016.restest/src/at/fhv/ecss2016/restest/parts/ConfigPart.java
+++ at.fhv.ecss2016.restest/src/at/fhv/ecss2016/restest/parts/ConfigPart.java
@@ -1,4 +1,3 @@
- 
 package at.fhv.ecss2016.restest.parts;
 
 import javax.annotation.PostConstruct;
@@ -12,8 +11,11 @@ import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
@@ -26,6 +28,9 @@ public class ConfigPart {
 	private static final String CREATABLE_PART_ID = "at.fhv.ecss2016.homework.task3.todo.partdescriptor.todo.edit";
 	private static final String POPUP_MENU_ID = "at.fhv.ecss2016.homework.task3.todo.popupmenu.listMenu";
 	
+	private static final int ELEMENT_VERTICAL_SPACING = 5;
+	private static final int ELEMENT_HORISONTAL_SPACING = 15;
+	
 	@Inject
 	public MDirtyable _dirty;
 	
@@ -36,27 +41,57 @@ public class ConfigPart {
 	@PostConstruct
 	public void postConstruct(Composite parent, EMenuService menuService, EPartService partService, EModelService modelService, MPerspective perspective) {
 		
-		parent.setLayout(new GridLayout(2, false));
+		// Setting parent layout
+		GridLayout gridLayout = new GridLayout(2, false);
+		gridLayout.verticalSpacing = ELEMENT_VERTICAL_SPACING;
+		gridLayout.horizontalSpacing = ELEMENT_HORISONTAL_SPACING;
 		
-		Label lblURL = new Label(parent, SWT.NONE);
-		lblURL.setText("URL:");
-		new Text(parent, SWT.BORDER);
+		parent.setLayout(gridLayout);
 		
-		Label lblVerb = new Label(parent, SWT.NONE);
-		lblVerb.setText("Verb:");
-		new Combo(parent, SWT.NONE);
+		// Setting parent font
+		FontData initFontData = parent.getFont().getFontData()[0];
 		
-		Label lblContentType = new Label(parent, SWT.NONE);
-		lblContentType.setText("Content-Type:");
-		new Combo(parent, SWT.NONE);
+		FontData fontData = new FontData(initFontData.getName(), initFontData.getHeight(), SWT.BOLD);
+		Font defaultFont = new Font(parent.getDisplay(), fontData);
+		
+		// Setting UI elements
+
+		Label urlLabel = new Label(parent, SWT.NONE);
+		urlLabel.setText("URL:");
+		urlLabel.setFont(defaultFont);
+		
+		Text urlText = new Text(parent, SWT.BORDER);
+		urlText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label verbLabel = new Label(parent, SWT.NONE);
+		verbLabel.setText("Verb:");
+		verbLabel.setFont(defaultFont);
+		
+		Combo verbCombo = new Combo(parent, SWT.NONE);
+		verbCombo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		
+		Label contentTypeLabel = new Label(parent, SWT.NONE);
+		contentTypeLabel.setText("Content-Type:");
+		contentTypeLabel.setFont(defaultFont);
+		
+		Combo contentTypeCombo = new Combo(parent, SWT.NONE);
+		contentTypeCombo.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		
 		Label separator = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
-		separator.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+		separator.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
 		
-		Label lblBody = new Label(parent, SWT.NONE);
-		lblBody.setText("Body:");
+		Label bodyLabel = new Label(parent, SWT.NONE);
+		bodyLabel.setText("Body:");
+		bodyLabel.setFont(defaultFont);
+		
 		StyledText styledText = new StyledText(parent, SWT.BORDER);
-		styledText.setLayoutData(new GridData(SWT.LEFT, SWT.CENTER, false, false, 2, 1));
+		styledText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+		
+		Button button = new Button(parent, SWT.NONE);
+		button.setText("Send");
+		button.setLayoutData(new GridData(SWT.RIGHT, SWT.FILL, true, false, 2, 1));
+		
+		
 		
 //		_tableViewer = new TableViewer(parent, SWT.FULL_SELECTION | SWT.MULTI);
 //		menuService.registerContextMenu(_tableViewer.getControl(), POPUP_MENU_ID);
