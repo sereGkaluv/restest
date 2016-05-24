60ec5d9aebe00a3b503452c0ffd773742b8db378
CarolaFHV <carola.nagel@students.fhv.at>
2016-05-24 11:16:51 +0200

    updated new config part, added file selector part

---
+++ at.fhv.ecss2016.restest/src/at/fhv/ecss2016/restest/parts/FileSelectorPart.java
@@ -0,0 +1,20 @@
+
+package at.fhv.ecss2016.restest.parts;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
+
+public class FileSelectorPart {
+	@Inject
+	public FileSelectorPart() {
+
+	}
+
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+		
+	}
+
+}
