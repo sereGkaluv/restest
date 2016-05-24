5138e85d2b605903f53859b226be6f0753b7e8c0
msmith <msmith@172.22.32.23>
2016-05-24 16:15:02 +0200

    Clean

---
+++ at.fhv.ecss2016.restest/src/at/fhv/ecss2016/restest/handlers/LoadConfigHandler.java
@@ -0,0 +1,11 @@
+ 
+package at.fhv.ecss2016.restest.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class LoadConfigHandler {
+	@Execute
+	public void execute() {
+		
+	}	
+}
