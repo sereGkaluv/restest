17d799b1f37f9526087cda6a2ad9d91451017fcc
sereGkaluv <malicx7@gmail.com>
2016-05-25 00:01:48 +0200

    before merge

--- at.fhv.ecss2016.restest/src/at/fhv/ecss2016/restest/parts/ConfigPart.java
+++ at.fhv.ecss2016.restest/src/at/fhv/ecss2016/restest/parts/ConfigPart.java
@@ -4,13 +4,16 @@ import java.io.IOException;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
+import javax.inject.Named;
 
+import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Persist;
 import org.eclipse.e4.ui.model.application.ui.MDirtyable;
 import org.eclipse.e4.ui.model.application.ui.advanced.MPerspective;
 import org.eclipse.e4.ui.model.application.ui.basic.MPart;
 import org.eclipse.e4.ui.model.application.ui.basic.MPartStack;
 import org.eclipse.e4.ui.services.EMenuService;
+import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.e4.ui.workbench.modeling.EModelService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService;
 import org.eclipse.e4.ui.workbench.modeling.EPartService.PartState;
@@ -33,9 +36,11 @@ import org.eclipse.swt.widgets.Listener;
 import org.eclipse.swt.widgets.Text;
 
 import at.fhv.ecss2016.restest.controller.RemoteConnection;
+import at.fhv.ecss2016.restest.model.Config;
 import at.fhv.ecss2016.restest.model.ContentType;
 import at.fhv.ecss2016.restest.model.HttpVerb;
 import at.fhv.ecss2016.restest.model.Response;
+import at.fhv.ecss2016.restest.util.BindHelper;
 import at.fhv.ecss2016.restest.util.StringConstants;
 
 /**
@@ -48,8 +53,15 @@ public class ConfigPart {
 	private static final String CREATABLE_PART_ID = "at.fhv.ecss2016.restest.partdescriptor.response";
 	private static final String RIGHT_PART_STACK_ID = "at.fhv.ecss2016.restest.config.partstack.right";
 	
+	private static final String URL_ATTRIBUTE = "URL_ATTRIBUTE";
+	private static final String VERB_ATTRIBUTE = "VERB_ATTRIBUTE";
+	private static final String CONTENT_TYPE_ATTRIBUTE = "CONTENT_TYPE_ATTRIBUTE";
+	private static final String CONTENT_BODY_ATTRIBUTE = "CONTENT_BODY_ATTRIBUTE";
+	
 	private static final int ELEMENT_VERTICAL_SPACING = 5;
-	private static final int ELEMENT_HORISONTAL_SPACING = 15;
+	private static final int ELEMENT_HORIZONTAL_SPACING = 15;
+	
+	private static final BindHelper BIND_HELPER = new BindHelper();
 	
 	@Inject
 	public MDirtyable _dirty;
@@ -64,7 +76,7 @@ public class ConfigPart {
 		// Setting parent layout
 		GridLayout gridLayout = new GridLayout(2, false);
 		gridLayout.verticalSpacing = ELEMENT_VERTICAL_SPACING;
-		gridLayout.horizontalSpacing = ELEMENT_HORISONTAL_SPACING;
+		gridLayout.horizontalSpacing = ELEMENT_HORIZONTAL_SPACING;
 		
 		parent.setLayout(gridLayout);
 		
@@ -81,6 +93,7 @@ public class ConfigPart {
 		
 		Text urlText = new Text(parent, SWT.BORDER);
 		urlText.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		BIND_HELPER.bindWidget(URL_ATTRIBUTE, urlText);
 		
 		Label verbLabel = new Label(parent, SWT.NONE);
 		verbLabel.setText("Verb:");
@@ -97,6 +110,7 @@ public class ConfigPart {
 			}
 		});
 		verbCombo.setInput(HttpVerb.values());
+		BIND_HELPER.bindViewer(VERB_ATTRIBUTE, verbCombo);
 		
 		Label contentTypeLabel = new Label(parent, SWT.NONE);
 		contentTypeLabel.setText("Content-Type:");
@@ -113,6 +127,7 @@ public class ConfigPart {
 			}
 		});
 		contentTypeCombo.setInput(ContentType.values());
+		BIND_HELPER.bindViewer(CONTENT_TYPE_ATTRIBUTE, contentTypeCombo);
 		
 		Label separator = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
 		separator.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
@@ -121,17 +136,17 @@ public class ConfigPart {
 		bodyLabel.setText("Body:");
 		bodyLabel.setFont(defaultFont);
 		
-		StyledText styledText = new StyledText(parent, SWT.BORDER);
-		styledText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+		StyledText contentBodyText = new StyledText(parent, SWT.BORDER);
+		contentBodyText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+		BIND_HELPER.bindWidget(CONTENT_BODY_ATTRIBUTE, contentBodyText);
 		
-		Button button = new Button(parent, SWT.NONE);
-		button.setText("Send");
-		button.setLayoutData(new GridData(SWT.RIGHT, SWT.FILL, true, false, 2, 1));
+		Button sendButton = new Button(parent, SWT.NONE);
+		sendButton.setText("Send");
+		sendButton.setLayoutData(new GridData(SWT.RIGHT, SWT.FILL, true, false, 2, 1));
 		
-		button.addListener(SWT.Selection, new Listener() {
+		sendButton.addListener(SWT.Selection, new Listener() {
 			@Override
-			public void handleEvent(Event event) {
-				
+			public void handleEvent(Event event) {				
 				display.asyncExec(() ->  {
 					// Reading data from UI
 					String uri = urlText.getText();
@@ -142,7 +157,7 @@ public class ConfigPart {
 					IStructuredSelection cSelection = (IStructuredSelection) contentTypeCombo.getSelection();
 					ContentType contentType = (cSelection != null && !cSelection.isEmpty()) ? (ContentType) cSelection.getFirstElement() : null;
 					
-					String body = styledText.getText();
+					String body = contentBodyText.getText();
 					
 					try {
 						
@@ -150,14 +165,58 @@ public class ConfigPart {
 						Response response = sendNewRequest(uri, httpVerb, contentType, body);
 						openResponsePerspective(response, perspective, partService, modelService);
 						
-					} catch (Exception e) { e.printStackTrace(); }
+					} catch (IllegalArgumentException | IOException e) { e.printStackTrace(); }
 				});
 			}
 		});
 	}
 	
+	@Inject
+	@Optional
+	private void setModel(@Named(IServiceConstants.ACTIVE_SELECTION) Config config) {
+		if (config != null) {			
+			// Set new values for the map entries from a model object
+			BIND_HELPER.updateAttributeValue(URL_ATTRIBUTE, config.getRequestURL());
+			BIND_HELPER.updateAttributeValue(VERB_ATTRIBUTE, config.getVerb());
+			BIND_HELPER.updateAttributeValue(CONTENT_TYPE_ATTRIBUTE, config.getContentType());
+			BIND_HELPER.updateAttributeValue(CONTENT_BODY_ATTRIBUTE, config.getRequestBody());
+	    }
+	}
+	
+	@Inject
+	private void setSelection(@Optional @Named(IServiceConstants.ACTIVE_SELECTION) Config config) {
+		if (config != null) setModel(config);
+	}
+	
+	@Persist
+	private void save() {
+//	    try {
+//	    	
+//	    	Resource resource = new ResourceSetImpl().createResource(URI.createURI("todoList/myList"));
+//		    resource.getContents().addAll(_uiEntries);
+//	
+//		    // Save the content
+//		    resource.save(Collections.EMPTY_MAP);
+//			_dirty.setDirty(false);
+//
+//	    } catch (IOException e) {
+//	    	e.printStackTrace();
+//	    }
+    }
+	
+	/**
+	 * Helper method that sends HTTP request.
+	 * 
+	 * @param uri where request will be send.
+	 * @param httpVerb request method.
+	 * @param contentType type of the content if available
+	 * @param body (content) of the request.
+	 * @return instance of {@code Response}, containing response information for produced request.
+	 * @throws IllegalArgumentException
+	 * @throws IOException
+	 */
 	private Response sendNewRequest(String url, HttpVerb httpVerb, ContentType contentType, String body)
-	throws IOException {
+	throws IllegalArgumentException, IOException {
 		RemoteConnection remoteConnection = new RemoteConnection();
 		
 		if (httpVerb != null) {
@@ -172,6 +231,14 @@ public class ConfigPart {
 		return null;
 	}
 	
+	/**
+	 * Helper method that opens response perspective.
+	 * 
+	 * @param response response to be displayed in new part.
+	 * @param perspective current perspective.
+	 * @param partService service for part management.
+	 * @param modelService service for fetching elements by id.
+	 */
 	private void openResponsePerspective(Response response, MPerspective perspective, EPartService partService, EModelService modelService) {
 		
 		// Forwarding response to the response part
@@ -188,20 +255,4 @@ public class ConfigPart {
 		
 		partService.showPart(editPart, PartState.ACTIVATE);
 	}
-	
-	@Persist
-	public void save() {
-//	    try {
-//	    	
-//	    	Resource resource = new ResourceSetImpl().createResource(URI.createURI("todoList/myList"));
-//		    resource.getContents().addAll(_uiEntries);
-//	
-//		    // Save the content
-//		    resource.save(Collections.EMPTY_MAP);
-//			_dirty.setDirty(false);
-//
-//	    } catch (IOException e) {
-//	    	e.printStackTrace();
-//	    }
-    }
 }
