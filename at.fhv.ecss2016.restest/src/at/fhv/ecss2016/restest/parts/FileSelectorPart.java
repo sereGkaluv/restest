ad02779646995f9be4568b4a9d692b12d1040187
CarolaFHV <carola.nagel@students.fhv.at>
2016-05-24 12:39:23 +0200

    updated dialogs & handlers, added responsy type enum, updated model

--- at.fhv.ecss2016.restest/src/at/fhv/ecss2016/restest/parts/FileSelectorPart.java
+++ at.fhv.ecss2016.restest/src/at/fhv/ecss2016/restest/parts/FileSelectorPart.java
@@ -4,7 +4,6 @@ package at.fhv.ecss2016.restest.parts;
 import javax.inject.Inject;
 import javax.annotation.PostConstruct;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.FileDialog;
 
 public class FileSelectorPart {
 	@Inject
--- at.fhv.ecss2016.restest/src/at/fhv/ecss2016/restest/parts/NewConfigPart.java
+++ at.fhv.ecss2016.restest/src/at/fhv/ecss2016/restest/parts/NewConfigPart.java
@@ -1,6 +1,10 @@
 
 package at.fhv.ecss2016.restest.parts;
 
+import org.apache.http.HttpStatus;
+import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.ComboViewer;
+import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
@@ -10,6 +14,8 @@ import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
+
+import at.fhv.ecss2016.restest.model.ContentType;
 
 public class NewConfigPart {
 
@@ -32,7 +38,7 @@ public class NewConfigPart {
 		cFileChoose.setLayout(new RowLayout());
 		Text txtCfile = new Text(cFileChoose, SWT.BORDER);
 		txtCfile.setMessage("Select a file");
-		Button chooseFileBtn = new Button(cFileChoose, 1);
+		Button chooseFileBtn = new Button(cFileChoose, SWT.PUSH);
 		chooseFileBtn.setText("Choose file...");
 
 		Label separator = new Label(parent, SWT.SEPARATOR | SWT.HORIZONTAL);
@@ -40,11 +46,31 @@ public class NewConfigPart {
 
 		Label lblResType = new Label(parent, SWT.NONE);
 		lblResType.setText("Result type: ");
-		new Combo(parent, 0);
+
+		ComboViewer contentTypeCombo = new ComboViewer(parent, SWT.DROP_DOWN | SWT.READ_ONLY);
+		contentTypeCombo.getCombo().setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
+		contentTypeCombo.setContentProvider(new ArrayContentProvider());
+		contentTypeCombo.setLabelProvider(new LabelProvider() {
+			@Override
+			public String getText(Object element) {
+				if (element instanceof ContentType) return ((ContentType) element).getName();
+				else return super.getText(element);
+			}
+		});
+		contentTypeCombo.setInput(ContentType.values());
+
+		Button containsBodyBtn = new Button(parent, SWT.CHECK);
+		containsBodyBtn.setText("Contains body");
 
 		Label lblResBody = new Label(parent, SWT.NONE);
 		lblResBody.setText("Result body: ");
 		new Text(parent, SWT.BORDER);
+
+		Button addBtn = new Button(parent, SWT.PUSH);
+		addBtn.setText("Add");
+
+		Button cancelBtn = new Button(parent, SWT.PUSH);
+		cancelBtn.setText("Cancel");
 	}
 
 }
